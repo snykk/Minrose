adaf26aa6b1e9487786e20295aed98944440f02c
Najib Fikri <najibfikri13@gmail.com>
2022-04-20 18:38:42 +0700

    fix false positive

--- system/core/Output.php
+++ system/core/Output.php
@@ -453,8 +453,9 @@ class CI_Output {
 
 		if ($this->parse_exec_vars === TRUE)
 		{
-			$memory	= round(memory_get_usage() / 1024 / 1024, 2).'MB';
-			$output = str_replace(array('{elapsed_time}', '{memory_usage}'), array($elapsed, $memory), $output);
+			//default: tanpa komen
+			// $memory	= round(memory_get_usage() / 1024 / 1024, 2).'MB';
+			// $output = str_replace(array('{elapsed_time}', '{memory_usage}'), array($elapsed, $memory), $output);
 		}
 
 		// --------------------------------------------------------------------
