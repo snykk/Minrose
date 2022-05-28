a4481f0d2fc6d1bd689176f776b45911eeab4ef9
Najib Fikri <najibfikri13@gmail.com>
2022-05-28 22:21:02 +0700

    remove unnecessary old code

--- application/views/admin/index.php
+++
@@ -1,4 +0,0 @@
-<div class="container">
-    <h1>Hallo ini home admin</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis qui nulla molestias atque, quam dolores molestiae explicabo voluptatem consequuntur doloremque sequi adipisci architecto expedita veniam ullam? Aliquid non iste fugiat?</p>
-</div>
